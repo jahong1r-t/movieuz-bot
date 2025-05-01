e757a3b8c4184ebf14be92e44d1c48e4ab6dbdaf
jahong1r-t <jahongirtorayev1507@gmail.com>
2025-05-01 14:03:04 +0500

    Version 2.0.0

--- src/main/java/com/github/jahong1r_t/exceptions/InvalidChannelLinkException.java
+++
@@ -1,7 +0,0 @@
-package com.github.jahong1r_t.exceptions;
-
-public class InvalidChannelLinkException extends Throwable {
-    public InvalidChannelLinkException(String s) {
-    }
-}
-
--- src/main/java/com/github/jahong1r_t/utils/BotConfig.java
+++ src/main/java/com/github/jahong1r_t/utils/BotConfig.java
@@ -4,7 +4,7 @@ public interface BotConfig {
     String BOT_TOKEN = System.getenv("BOT_TOKEN");
     String BOT_USERNAME = System.getenv("BOT_USERNAME");
     Long BOT_ADMIN_1 = Long.parseLong(System.getenv("BOT_ADMIN_1"));
-    Long BOT_ADMIN_2 = Long.parseLong(System.getenv("BOT_ADMIN_1"));
+    Long BOT_ADMIN_2 = Long.parseLong(System.getenv("BOT_ADMIN_2"));
     String DB_URL = System.getenv("DB_URL");
     String DB_USER = System.getenv("DB_USER");
     String DB_PASS = System.getenv("DB_PASS");
